d86c2b4da47bc9a01d95aec9ad10dc38f46d1f28
mcardacci <marc.cardacci@gmail.com>
2015-10-09 17:41:29 -0400

    adds template views and controllers w locProvider

--- public/app.js
+++ public/app.js
@@ -1,8 +1,31 @@
 angular.module('MyApp', ['ngCookies', 'ngResource', 'ngMessages', 'ngRoute', 'mgcrea.ngStrap'])
-	.config(function() {
-		// service that configures app linking paths, useful for disquis
+		// service that configures how the apps deep linking paths are stored, useful for disquis and much more
+	.config(['$locationProvider', '$routeProvider', function($locationProvider, $routeProvider) {
 		$locationProvider.html5Mode(true);
 
-		
-	});
+		$routeProvider
+		.when('/', {
+			templateUrl: 'views/home.html',
+			controller: 'MainCtrl'
+		})
+		.when('/shows/:id', {
+			templateUrl: 'views/detail.html',
+			controller: 'DetailCtrl'
+		})
+		.when('/login', {
+			templateUrl: 'views/login.html',
+			controller: 'LoginCtrl'
+		})
+		.when('/signup', {
+			templateUrl: 'views/signup.html',
+			controller: 'SignupCtrl'
+		})
+		.when('/add', {
+			templateUrl: 'views/add.html',
+			controller: 'AddCtrl'
+		})
+		.otherwise({
+			redirectTo: '/'
+		});
+	}]);
 
