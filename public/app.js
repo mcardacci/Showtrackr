5597ddb8280fabfd102a816d696e5d79af8fa6d8
mcardacci <marc.cardacci@gmail.com>
2015-10-09 15:10:56 -0400

    comments on location provider

--- public/app.js
+++ public/app.js
@@ -1,5 +1,6 @@
 angular.module('MyApp', ['ngCookies', 'ngResource', 'ngMessages', 'ngRoute', 'mgcrea.ngStrap'])
 	.config(function() {
+		// service that configures app linking paths, useful for disquis
 		$locationProvider.html5Mode(true);
 
 		
