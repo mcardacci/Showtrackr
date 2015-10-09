d6b49cd9148611b935c38e83bf33332e6f750a51
mcardacci <marc.cardacci@gmail.com>
2015-10-09 13:22:10 -0400

    makes gulp watch for file changes, compile sass, and adds plumber

---
+++ gulpfile.js
@@ -0,0 +1,20 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var plumber = require('gulp-plumber');
+
+// Gulp Plumber - It will prevent pipe breaking caused by errors from 
+// gulp plugins. In other words when you make a syntax 
+// error in a Sass stylesheet, the gulp watcher will not crash
+
+gulp.task('sass', function() {
+	gulp.src('public/stylesheets/style.scss')
+	.pipe(plumber())
+	.pipe(sass())
+	.pipe(gulp.dest('public/stylesheets'));
+});
+
+gulp.task('watch', function() {
+	gulp.watch('public/sylesheets/*.scss', ['sass']);
+});
+
+gulp.task('default', ['sass', 'watch']);
