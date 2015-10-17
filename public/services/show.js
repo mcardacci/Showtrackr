b6e2f3a87c31b935f528d9a2dbe5042b229f35b3
mcardacci <marc.cardacci@gmail.com>
2015-10-17 16:47:35 -0400

    creates dtail controller

--- public/services/show.js
+++ public/services/show.js
@@ -1,6 +1,15 @@
 angular.module('MyApp')
 //$resource is a factory that crates a resource object tthatt letts you
 //interact with RESTful server-side data sources
+//angular provides factories so we don't handle http
+//requests inside controllers. Keeps code modular
 	.factory('Show', ['$resource', function($resource) {
 		return $resource('/api/shows/:id');
 	}]);		
+// //By default this service has the following methods.
+// { 'get':    {method:'GET'},
+//   'save':   {method:'POST'},
+//   'query':  {method:'GET', isArray:true},
+//   'remove': {method:'DELETE'},
+//   'delete': {method:'DELETE'} };
+
