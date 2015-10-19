b40c8bcad651bbf40ea78c9c92d57220806847f0
mcardacci <marc.cardacci@gmail.com>
2015-10-19 16:50:22 -0400

    adds subscription service

--- public/controllers/detail.js
+++ public/controllers/detail.js
@@ -1,31 +1,31 @@
 angular.module('MyApp')
-.controller('DetailCtrl', ['$scope', '$rootScope', '$routeParams', 'Show', 'Subscription',
-  function($scope, $rootScope, $routeParams, Show, Subscription) {
-    Show.get({ _id: $routeParams.id }, function(show) {
-      $scope.show = show;
+  .controller('DetailCtrl', function($scope, $rootScope, $routeParams, Show, Subscription) {
+      Show.get({ _id: $routeParams.id }, function(show) {
+        $scope.show = show;
 //when we get a response from the Show service  
 //above we add it to scope in order to make it 
 //to detail.html
       $scope.isSubscribed = function() {
-        return $scope.show.subscribers.indexOf($rootScope.currentUser._id) !== -1;
-      };
+          return $scope.show.subscribers.indexOf($rootScope.currentUser._id) !== -1;
+        };
       // pushes user id into subscriber array in the subscriber model
       $scope.subscribe = function() {
-        Subscription.subscribe(show).success(function() {
-          $scope.show.subscribers.push($rootScope.currentUser._id);
-        });
-      };
+          Subscription.subscribe(show).success(function() {
+            $scope.show.subscribers.push($rootScope.currentUser._id);
+          });
+        };
 
       $scope.unsubscribe = function() {
-        Subscription.unsubscribe(show).success(function() {
-          var index = $scope.show.subscribers.indexOf($rootScope.currentUser._id);
-          $scope.show.subscribers.splice(index, 1);
-        });
-      };
+          Subscription.unsubscribe(show).success(function() {
+            var index = $scope.show.subscribers.indexOf($rootScope.currentUser._id);
+            $scope.show.subscribers.splice(index, 1);
+          });
+        };
+
 //filter() method goes through every episode and checks if
 //it passes the 'new Date(episode.firstAired)' condition
-      $scope.nextEpisode = show.episodes.filter(function(episode) {
-        return new Date(episode.firstAired) > new Date();
-      })[0];
-    });
-  }]);
+       $scope.nextEpisode = show.episodes.filter(function(episode) {
+          return new Date(episode.firstAired) > new Date();
+        })[0];
+      });
+    }]);
