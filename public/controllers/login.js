15774fd333b9103d0922f333e0c700706dea43a5
mcardacci <marc.cardacci@gmail.com>
2015-10-22 17:54:22 -0400

    modifies app config

--- public/controllers/login.js
+++ public/controllers/login.js
@@ -1,9 +1,15 @@
 angular.module('MyApp')
-	.controller('LoginCtrl', ['$scope', 'Auth', function($scope, Auth) {
-		$scope.login = function() {
-			Auth.login({
-				email: $scope.email,
-				password: $scope.password
-			});
-		};
-	}]);
+  .controller('LoginCtrl', function($scope, Auth) {
+    $scope.login = function() {
+      Auth.login({ 
+      	email: $scope.email, 
+      	password: $scope.password });
+    };
+    $scope.facebookLogin = function() {
+      Auth.facebookLogin();
+    };
+    $scope.googleLogin = function() {
+      Auth.googleLogin();
+    };
+    $scope.pageClass = 'fadeZoom';
+  });
