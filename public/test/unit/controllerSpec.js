41316e3ad40911f321dd9b9926303f5a591624f8
mcardacci <marc.cardacci@gmail.com>
2015-10-25 22:42:36 -0400

    fixes it declaration

--- public/test/unit/controllerSpec.js
+++ public/test/unit/controllerSpec.js
@@ -2,7 +2,7 @@ describe('controllers', function(){
   beforeEach(module('MyApp'));
 
   describe('AddCtrl', function() {
-    it('should add a new show', inject(function($controller) {
+    it('should be defined', inject(function($controller) {
       var addCtrl = $controller('AddCtrl', { $scope: {} });
       expect(addCtrl).toBeDefined();
     }));
